9d00cab957e35983959f3deaeaa5631e81b4c3aa
Arne Brasseur <arne@arnebrasseur.net>
2017-04-25 17:59:22 +0200

    Try to get Travis to do something

--- project.clj
+++ project.clj
@@ -1,18 +1,23 @@
-(defproject lambdaisland/uniontypes "0.1.0"
+(defproject lambdaisland/uniontypes "0.2.0"
   :description "Union Types (ADTs) based on clojure.spec"
   :url "http://github.com/lambdaisland/uniontypes"
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
-                 [org.clojure/clojurescript "1.9.229" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.521" :scope "provided"]]
 
   :doo {:build "test"}
 
   :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js/compiled"]
 
+  :source-paths ["src"]
+  :test-paths ["test"]
+
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-doo "0.1.6"]]
+
+  :min-lein-version "2.5.1"
 
   :cljsbuild {:builds
               {:test
@@ -25,4 +30,6 @@
 
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
 
-  :aliases {"test-all" ["do" "test" ["doo" "phantom" "test" "once"]]})
+  :aliases {"test-all" ["do"
+                        ["test"]
+                        ["doo" "phantom" "test" "once"]]})
